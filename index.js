c460e94b09e548a693047a6b5d475975cb7ed430
CMING <cming.pms@gmail.com>
2022-11-10 17:07:38 +0800

    removed create table function. gameBoard and displayController function on process

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-function drawBoard(){
-    const table = document.querySelector("table")
-    for (let i = 0; i < 3; i++){
-        const tr = document.createElement("tr")
-        table.appendChild(tr)
-        for (let i = 0; i < 3; i++){
-            const td = document.createElement("td")
-            tr.appendChild(td)
-        }
-    }
+const domController = () => {
+    
 }
-drawBoard()
+
+
+const gameBoard = () => {
+    const board = new Array(9)
+    //Check player
+    //Place mark
+    //Check win
+    //Change turn
+    const setboard = (index, sign) => {
+
+    }
+    
+}
+
+//player constructor
+const player = (mark, turn) => {
+     this.mark = mark;
+     this.turn = turn;
+}
+
+// 
+const displayController = (()=>{
+    const cells = document.querySelectorAll(".cell")
+    cells.forEach("click", (e)=>{
+        if (e.target.innerHTML !== "") return;
+        setboard()
+    })
+    
+})()
