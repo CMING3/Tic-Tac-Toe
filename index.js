75559129c7e55f9226ea22b8026d997bff95b7d6
CMING <cming.pms@gmail.com>
2022-11-14 10:41:57 +0800

    changed player constructor, added gameController, testing player's changeTurn function

--- index.js
+++ index.js
@@ -1,32 +1,59 @@
-const domController = () => {
-    
-}
-
-
-const gameBoard = () => {
+const gameBoard = (() => {
+    //build array for game board
     const board = new Array(9)
-    //Check player
     //Place mark
-    //Check win
-    //Change turn
-    const setboard = (index, sign) => {
-
+    const setBoard = (index, sign) => {
+        console.log(`${sign} is putting in ${index}`)
     }
-    
-}
+    return{setBoard}
+})()
 
 //player constructor
-const player = (mark, turn) => {
+function player(mark, turn) {
      this.mark = mark;
      this.turn = turn;
+     this.changeTurn = () => {
+        this.turn = Boolean(this.turn ? "false" : "ture");
+        console.log(this)
+     }
 }
 
 // 
 const displayController = (()=>{
+    //get game board element
     const cells = document.querySelectorAll(".cell")
-    cells.forEach("click", (e)=>{
-        if (e.target.innerHTML !== "") return;
-        setboard()
+    cells.forEach((cell) => {
+        cell.addEventListener("click", (e)=>{
+            if (e.target.innerHTML !== "") return;
+            //start the game here!
+            gameController.playGame(e.target.id)
+        })
     })
-    
+})()
+
+const gameController = (() => {
+    //Check player
+    const playerO = new player("O", true);
+    const playerX = new player("X", false);
+    //Check win
+    const winRules = [  
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+                ];
+
+    const playGame = (id) => {
+        let sign = (playerO.turn===true) ? "O" : "X";
+        gameBoard.setBoard(id, sign)
+        playerO.changeTurn()
+        console.log(playerO)
+        playerX.changeTurn()
+    }
+
+    return {playGame};
 })()
